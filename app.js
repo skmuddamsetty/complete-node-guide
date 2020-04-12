b7eccf216426e465b6c47b51bc1757660f268cc1
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-12 09:07:21 -0400

    seperated global error handler into a file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -54,11 +55,6 @@ app.all('*', (req, res, next) => {
 
 // Global Error Handling Middleware
 // by adding 4 parameters express knows that this is the error handling middleware
-app.use((err, req, res, next) => {
-  // 500 Internal Server Error
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({ status: err.status, message: err.message });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
