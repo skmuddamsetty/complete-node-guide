185b2b2fb3fd88d1328f2a95a3f04bbaedf1ecac
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-13 23:44:00 -0400

    completed token generation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,12 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res) => {
   const newUser = await User.create(req.body);
-  res.status(201).json({ status: 'success', data: { user: newUser } });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  res.status(201).json({ status: 'success', token, data: { user: newUser } });
 });
