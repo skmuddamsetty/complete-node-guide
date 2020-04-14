4f63708133ec7be6d03fe8b9e7ceaa681d4e4763
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-13 23:11:21 -0400

    completed signup

---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter a valid email id!'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please provide passwordConfirm'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router
   .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
+
 router
   .route('/:id')
   .get(userController.getUser)
