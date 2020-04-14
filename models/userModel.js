3671ac464591567e9dd1660020cb8697eafac3a3
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-13 23:35:30 -0400

    completed encrypting the password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,8 +23,25 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please provide passwordConfirm'],
+    validate: {
+      // this only works on .create() or .save()!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not matching!',
+    },
   },
 });
 
+userSchema.pre('save', async function (next) {
+  // this keyword refers to the current document and we are checking if the password has been modified
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  // setting this to undefined will not persist this field to database
+  this.passwordConfirm = undefined;
+  next();
+});
+
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
