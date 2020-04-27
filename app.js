28192ad725eb299ed834cc325122e552291b883f
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-27 17:20:39 -0400

    completed extending base template to tour and overview page

--- app.js
+++ app.js
@@ -98,6 +98,14 @@ app.get('/', (req, res) => {
   res.status(200).render('base', { tour: 'The Forest Hiker', user: 'John' });
 });
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', { title: 'All Tours' });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', { title: 'The Forest Hiker' });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/books', bookRouter);
