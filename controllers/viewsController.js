3f19d7aa1a94cff80b872341b49cbfa03c6cb480
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-04-28 07:42:52 -0400

    completed building tour page with errors in rendering images and stars

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
-const handler = require('./handlerFactory');
+// const handler = require('./handlerFactory');
 
 exports.getOverview = catchAsync(async (req, res) => {
   // 1) get tour data from our collection
@@ -11,6 +11,13 @@ exports.getOverview = catchAsync(async (req, res) => {
   res.status(200).render('overview', { title: 'All Tours', tours });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', { title: 'The Forest Hiker' });
-};
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get the data for the requested data (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    select: 'review rating user',
+  });
+  // 2) Build a Template
+  // 3) Render Template Using Data from Step 1
+  res.status(200).render('tour', { tour });
+});
