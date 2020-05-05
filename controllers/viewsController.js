d6424f87ae4245a032c7c271c2f12056b0daacdd
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-04 22:11:48 -0400

    completed error handling

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 // const handler = require('./handlerFactory');
 
 exports.getOverview = catchAsync(async (req, res) => {
@@ -10,12 +11,15 @@ exports.getOverview = catchAsync(async (req, res) => {
   res.status(200).render('overview', { title: 'All Tours', tours });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data for the requested data (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     select: 'review rating user',
   });
+  if (!tour) {
+    return next(new AppError('There is no tour with that name', 404));
+  }
   // 2) Build a Template
   // 3) Render Template Using Data from Step 1
   res.status(200).render('tour', { tour });
